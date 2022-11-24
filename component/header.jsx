d5658233946417f9a76fc2f355b81d7b52de312a
Preet Kaur <117669131+preetkaur13@users.noreply.github.com>
2022-11-24 16:53:08 +0530

    Initial commit

---
+++ component/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <Header>
+      <h1>Default Header</h1>
+    </Header>
+  );
+}
+
+export default Header;
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const ul = ul.createElement('ul');
+const li = li.createElement('li');
+li.appendChild(document.createTextNode('Hey'))
+function App() {
+  return (
+    <div>
+      <header />
+      <div className="dummy">
+        </div>
+    </div>
+  );
+}
+export default App;
